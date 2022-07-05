1871795a2ef0ede21a011c71316aae68af6970df
Alex Harris <alexharrisemail@gmail.com>
2022-07-05 17:47:31 -0400

    adds some info on frontpage

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const hamburger = document.getElementById('hamburger');
 const navUL = document.getElementById('nav-ul');
+const name = document.getElementById('name');
+const title = document.getElementById('title')
+const passion = document.getElementById('passion')
 
 hamburger.addEventListener('click', () => {
     navUL.classList.toggle('show');
 });
+
