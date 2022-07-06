ef0184c5a3981cb6a80645b00fa32b69011e6ef3
Alex Harris <alexharrisemail@gmail.com>
2022-07-06 14:41:03 -0400

    adds styling to footer

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 const hamburger = document.getElementById('hamburger');
+const close = document.getElementById('close');
 const navUL = document.getElementById('nav-ul');
 const name = document.getElementById('name');
 const title = document.getElementById('title')
 const passion = document.getElementById('passion')
 
 hamburger.addEventListener('click', () => {
-    navUL.classList.toggle('show');
+    hamburger.classList.add('hide')
+    navUL.classList.add('show');
+    close.classList.remove('hide')
 });
+
+close.addEventListener('click', () => {
+    close.classList.add('hide');
+    hamburger.classList.remove('hide');
+    navUL.classList.remove('show');
+})
 
